33cf31dc6f76cef35d3bf4ef93fb99c9ab0df631
Rramadhan1818 <55430991+Rramadhan1818@users.noreply.github.com>
2020-10-18 16:31:13 +0700

    first commit

---
+++ src/if10k/pkg10119912/latihan15/logika/IF10K10119912Latihan15Logika.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119912.latihan15.logika;
+
+/**
+ *
+ * @author 
+ * NAMA  : Rezki Ramadhan
+ * KELAS : IF10K
+ * NIM  `: 10119912
+ * Deskripsi Program : Program ini berisi proses logika dengan boolean
+ * 
+ */
+public class IF10K10119912Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       boolean a = true;
+       boolean b = false;
+        System.out.println("a && b = " + (a && b));        
+        System.out.println("a || b = " + (a || b));        
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));        
+
+
+        
+    }
+    
+}
